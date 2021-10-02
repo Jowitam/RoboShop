0a74daae92115b23d25a8ad252b42fcd9f3d74f0
Jowitam <jowita.mielnicka@gmail.com>
2021-10-02 22:46:20 +0200

    walidacje na produkcie oraz zainicjowanie uplodera zdjec

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, numericality: {greater_than: 0.0 }
+  validates :category, presence: true
+
+  mount_uploader :photo, ProductPhotoUploader
 end
