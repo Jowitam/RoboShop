4895816c347a99a80fd520200519198e223e0793
Jowitam <jowita.mielnicka@gmail.com>
2021-10-03 17:02:47 +0200

    dodanie nazwy produktu/kategori do url

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
     has_many :products
 
     validates :name, presence: true
+
+    def to_param
+        "#{id}-#{name}".parameterize
+    end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,10 @@ class Product < ApplicationRecord
   validates :category, presence: true
 
   mount_uploader :photo, ProductPhotoUploader
+
+
+  def to_param
+    "#{id}-#{name}".parameterize
+end
+
 end
