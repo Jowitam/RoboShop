d77add8f3c623f8e4e46fd8eb0972d7a628f6e1e
Jowitam <jowita.mielnicka@gmail.com>
2021-10-03 16:57:42 +0200

    spolszenie nazwy urli dla produktow i kategori

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
 
   root to: "products#index"
-  resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :products, only: [:index, :show], path: "produkt"
+  resources :categories, only: [:show], path: "kategoria"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
