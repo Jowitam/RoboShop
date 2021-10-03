c7718ed99fd64235d64fc3784a74edebae0a55e0
Jowitam <jowita.mielnicka@gmail.com>
2021-10-03 12:14:18 +0200

    dodanie seed ow, ignore zdjec z bazy, poprawa uploadera

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+8.times do
+    Category.create! name: Faker::Hacker.ingverb.capitalize
+end
+
+category_ids = Category.pluck(:id)
+
+print "Product "
+200.times do
+    product = Product.create! name: "#{Faker::Hacker.verb} #{Faker::Hacker.noun}".capitalize,
+        description: Faker::Hacker.say_something_smart,
+        long_description: Faker::Lorem.paragraphs(3).join("\n\n"),
+        category_id: category_ids.sample,
+        price: Faker::Number.decimal(3,2)
+
+    product.remote_photo_url = Faker::Avatar.image(
+        slug: product.name.parameterize,
+        size: '640x480',
+        format:'jpg',
+        set: "#{[1,2,3].sample}", 
+        bgset: "#{[1,2].sample}"
+    )
+    product.save
+    print "."
+end
+puts
