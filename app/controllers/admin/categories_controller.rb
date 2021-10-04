eeb7d10e3c48f385a9ea59d3568b276afe9c72de
Jowitam <jowita.mielnicka@gmail.com>
2021-10-04 20:49:01 +0200

    kontrolery panelu admina kategorie i produkty

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,37 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create 
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path, notice: "Dodano kategorię."
+    else 
+      render action: :new
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update 
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to admin_categories_path, notice: "Zmieniono kategorię."
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,44 @@
 class Admin::ProductsController < Admin::BaseController
   def index
+    @products = Product.page(params[:page]).per(30)
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create 
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: "Dodano produkt."
+    else 
+      render action: :new
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update 
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to admin_products_path, notice: "Zmieniono produkt."
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :category_id,
+      :description,
+      :long_description,
+      :price,
+      :photo
+    )
   end
 end
