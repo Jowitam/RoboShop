ec169136f1c2fadcc5841e51d23c505f778dae9f
Jowitam <jowita.mielnicka@gmail.com>
2021-10-06 19:43:53 +0200

    dodanie walidacji na modelu address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  validates :first_name, :last_name, :zip_code, :city, :street, :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_203302) do
+ActiveRecord::Schema.define(version: 2021_10_06_173345) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "city"
+    t.string "zip_code"
+    t.string "street"
+    t.string "email"
+    t.integer "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_addresses_on_order_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.decimal "unit_price"
+    t.integer "quantity"
+    t.integer "order_id", null: false
+    t.string "item_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.text "comment"
+    t.integer "shipping_type_id", null: false
+    t.decimal "shipping_cost"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shipping_type_id"], name: "index_orders_on_shipping_type_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -30,5 +64,16 @@ ActiveRecord::Schema.define(version: 2021_10_02_203302) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "shipping_types", force: :cascade do |t|
+    t.string "name"
+    t.decimal "cost"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "addresses", "orders"
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
+  add_foreign_key "orders", "shipping_types"
   add_foreign_key "products", "categories"
 end
