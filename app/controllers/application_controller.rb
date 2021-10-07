cc981e0cea6af76356b2ddade180281ee9ca2573
Jowitam <jowita.mielnicka@gmail.com>
2021-10-07 18:29:35 +0200

    kontroler aktualnego koszyka z id pobieranym z sesi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def current_cart
+        if session[:order_id]
+            Order.find(session[:order_id])
+        else
+            Order.new
+        end
+    end
 end
