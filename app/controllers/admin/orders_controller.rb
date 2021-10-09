dfeda9676b56b391b3d5cf015535a7ef4c1d5891
Jowitam <jowita.mielnicka@gmail.com>
2021-10-09 18:18:48 +0200

    mozliwosc zmiany statusu zamowienia w panelu admina, dodatkowo - zmiana controlera order pod admina

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,15 @@
-class Admin::OrdersController < ApplicationController
+class Admin::OrdersController < Admin::BaseController
   def index
     @orders = Order.not_in_state(:new).page(params[:page]).per(20)
   end
 
   def show
     @order = Order.find(params[:id])
+  end
+
+  def update 
+    @order = Order.find(params[:id])
+    @order.transition_to params[:state]
+    redirect_to admin_order_path(@order), notice: "Pomyślnie zmieniono zamówienie."
   end
 end
